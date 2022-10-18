d81b2c5bb85ef825e0e15c86430351dc8be0534a
William <wms274@nyu.edu>
2022-10-18 11:41:02 -0400

    commit tree 07c5e5176d1e3ae8eb221b5c8c75189408be3c1a

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,31 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
 
+    def freebie_names
+        freebies.map do |f|
+            f.item_name
+        end
+    end
+
+    def received_one?(item_name)
+        freebie_names = []
+        self.freebies.map do |f|
+            freebie_names << f.item_name
+        end
+
+        if freebie_names.include?(item_name)
+            true
+        else
+            false
+        end
+    end
+
+    def give_away(dev, freebie)
+        if self.received_one?(freebie.item_name)
+            freebie.update(dev_id: dev.id)
+        else
+            "You can't give someone else's shit away"
+        end
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,13 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "hat", value: 2, company_id: 1, dev_id: 2)
+Freebie.create(item_name: "shirt", value: 3, company_id: 3, dev_id: 3)
+Freebie.create(item_name: "pants", value: 1, company_id: 2, dev_id: 4)
+Freebie.create(item_name: "mug", value: 4, company_id: 4, dev_id: 1)
+Freebie.create(item_name: "shotglass", value: 5, company_id: 4, dev_id: 2)
+Freebie.create(item_name: "waterbottle", value: 1, company_id: 2, dev_id: 3)
+Freebie.create(item_name: "sweatshirt", value: 1, company_id: 3, dev_id: 4)
+Freebie.create(item_name: "phonecase", value: 2, company_id: 1, dev_id: 1)
 
 puts "Seeding done!"
